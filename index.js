dfa79cccc2f83b8744a66f8c6f896fa80c85e496
JayeunPark <jayeunpark0704@gmail.com>
2023-08-02 23:56:00 +0900

    settings 3
    nodejs로 웹사이트를 만들기 위한 기초작업3

--- index.js
+++
@@ -1,6 +0,0 @@
-// express 사용방법과 express를 이용한 app 생성 방법
-// const express = require("express"); <= 옛날코드
-// import express from "express"; <= 최신코드 (babel이 있으면 이걸로 써도 실행가능)
-
-// const app = express();
-console.log("i'm fineeeee!!!!!!!!");
